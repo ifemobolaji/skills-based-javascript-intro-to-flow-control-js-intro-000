e2341e02a9322a77a29f9089f3e5fbec3876f97f
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-13 18:02:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,12 @@
 function basicTeenager(age) {
   if (age >= 13 && <= 19)
-  {return('You are a teenager!');}
+  {return'You are a teenager!';}
 
 };
 
 function teenager(age) {
-  if (age >=13 && <=19) {return ('You are a teenager!');}
-   else {return ('You are not a teenager!');}
+  if (age >=13 && <=19) {return 'You are a teenager!';}
+   else {return 'You are not a teenager!';}
 };
 
 function ageChecker(age) {
